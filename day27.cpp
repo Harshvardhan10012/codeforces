fb2c0e4fc9cdb88ac5828afaec6272298f7473c1
Harshvardhan10012 <harshvardhan90065@gmail.com>
2025-07-21 01:26:35 +0530

    day 27

---
+++ day27.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void harsh() {
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    int ans = 0;
+    for(int i = n - 2; i >= 0; i--){
+        while(a[i] >= a[i + 1] && a[i] > 0){
+            a[i] /= 2;
+            ans++;
+        }
+        if(a[i] == a[i+1]){
+            cout << -1 << '\n';
+            return;
+        }
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    #ifndef ONLINE_JUDGE 
+    freopen("input.txt", "r", stdin); 
+    freopen("output.txt", "w", stdout); 
+    #endif
+
+    int t;
+    cin >> t;
+    while (t--) {
+        harsh();
+    }
+
+    return 0;
+}
